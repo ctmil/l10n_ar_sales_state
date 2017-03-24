89bf386f0ae5c9a4229316897372ba12436b6f15
morrillo <gustavo.orrillo@gmail.com>
2017-03-24 11:52:06 -0300

    Fixed bug

--- account.py
+++ account.py
@@ -18,8 +18,8 @@ class account_invoice(osv.osv):
     def init(self, cr):
         tools.sql.drop_view_if_exists(cr, 'account_state_report')
 	cr.execute("""
-		create or replace view account_tax_vat_report as (
-				select a.company_id as company_id,a.period_id as period_id,a.journal_id as journal_id,b.state_id as state_id,sum(a.amount_total) as amount_total, 
+		create or replace view account_state_report as (
+				select a.id,a.company_id as company_id,a.period_id as period_id,a.journal_id as journal_id,b.state_id as state_id,sum(a.amount_total) as amount_total, 
 					sum(a.amount_untaxed) as amount_untaxed 
 					from account_invoice a inner join res_partner b on a.partner_id = b.id 
 					where a.state in ('open','paid') and a.type in ('out_invoice','out_refund') 
